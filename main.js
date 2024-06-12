e0120997b972c5ede70195f493d9a5d7c5e83fc9
jooanna4 <joana.bruguera@gmail.com>
2024-06-12 14:34:39 +0200

    cosetes

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+// BARRA LATERAL
 const menuIcon = document.getElementById("icono_barra_lateral");
 const barraLateral = document.getElementById("barra_lateral");
+
+// IMPORTAR NOTÍCIES
+import {noticies} from './noticiesArr.js';
 
 
 menuIcon.addEventListener('click', function () {
@@ -11,3 +15,29 @@ menuIcon.addEventListener('click', function () {
     barraLateral.style.pointerEvents = 'auto';
   }
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+  const contenedorNoticias = document.getElementById('noticies');
+  const templateCard = document.querySelector('.card-template');
+
+  if (templateCard) {
+    noticies.forEach((noticia) => {
+      // Crea una copia de la plantilla de la tarjeta
+      const card = templateCard.cloneNode(true);
+      card.classList.remove('card-template'); // Remove the template class
+
+      // Obte els elements dins la tarjeta
+      const cardDate = card.querySelector('.data');
+      const cardTitle = card.querySelector('h3');
+      const cardSummary = card.querySelector('.resumNoticia');
+
+      // Actualitza informació
+      cardDate.textContent = noticia.data;
+      cardTitle.textContent = noticia.titol;
+      cardSummary.textContent = noticia.resum;
+
+      // Añade la tarjeta al contenedor
+      contenedorNoticias.appendChild(card);
+    });
+  }
+});
---
+++ noticiesArr.js
@@ -0,0 +1,142 @@
+const noticies = [
+    {
+      id: 1,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 2,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 3,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 4,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 5,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 6,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 7,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 8,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 9,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 10,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 11,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 12,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 13,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 14,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 15,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 16,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 17,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 18,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 19,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    },
+    {
+      id: 20,
+      data: "Data",
+      titol: "Noticia 1",
+      resum: "Resum de la noticia 1",
+      noticiaCompleta: "Contenido completo de la noticia 1"
+    }
+];
