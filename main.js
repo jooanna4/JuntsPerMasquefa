6f55d14ae6cc212b5cef267d73f609b2fd9232f9
Joanna <102182949+jooanna4@users.noreply.github.com>
2024-06-13 18:15:40 +0200

    mm

--- main.js
+++ main.js
@@ -2,10 +2,6 @@
 const menuIcon = document.getElementById("icono_barra_lateral");
 const barraLateral = document.getElementById("barra_lateral");
 
-// IMPORTAR NOTÍCIES
-import {noticies} from './noticiesArr.js';
-
-
 menuIcon.addEventListener('click', function () {
   if (barraLateral.style.opacity == "1") {
     barraLateral.style.opacity = '0';
@@ -15,29 +11,3 @@ menuIcon.addEventListener('click', function () {
     barraLateral.style.pointerEvents = 'auto';
   }
 })
-
-document.addEventListener('DOMContentLoaded', function () {
-  const contenedorNoticias = document.getElementById('noticies');
-  const templateCard = document.querySelector('.card-template');
-
-  if (templateCard) {
-    noticies.forEach((noticia) => {
-      // Crea una copia de la plantilla de la tarjeta
-      const card = templateCard.cloneNode(true);
-      card.classList.remove('card-template'); // Remove the template class
-
-      // Obte els elements dins la tarjeta
-      const cardDate = card.querySelector('.data');
-      const cardTitle = card.querySelector('h3');
-      const cardSummary = card.querySelector('.resumNoticia');
-
-      // Actualitza informació
-      cardDate.textContent = noticia.data;
-      cardTitle.textContent = noticia.titol;
-      cardSummary.textContent = noticia.resum;
-
-      // Añade la tarjeta al contenedor
-      contenedorNoticias.appendChild(card);
-    });
-  }
-});
