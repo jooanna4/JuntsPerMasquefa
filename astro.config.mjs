7e1b26a596f2e3224474f72bf598a13b068a3dd7
Joanna <102182949+jooanna4@users.noreply.github.com>
2024-12-11 12:23:55 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jooanna4.github.io',
+    site: 'https://jooanna4.github.io/JuntsPerMasquefa',
     base: 'JuntsPerMasquefa',
 });
